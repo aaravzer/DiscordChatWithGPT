985e4d5f642adf5cad4221625ca06dcf23c9b65d
aaravq <71241229+aaravzer@users.noreply.github.com>
2022-12-23 18:05:18 -0500

    Add files via upload

---
+++ testbot.py
@@ -0,0 +1,36 @@
+import openai
+import discord
+import asyncio
+
+openai.api_key = "YOUR_API_KEY"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("------")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("!gpt3"):
+        prompt = message.content[6:]
+
+        completions = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+
+        response = completions.choices[0].text
+        await message.channel.send(response)
+
+client.run("MTA1NTkxODczODk2MjEyMDc1NA.Gs_5ys.uHp_fS8476v0dNenTY7q2qe6avPw7WWkwoYDaY")
